e7256d4fd4ef903dc95cf09cbcf6b4ea32d43930
abhishek332 <itsabhiofficial.21@gmail.com>
2022-08-14 16:25:11 +0530

    SignUp SignIn Flow Complete, redux logic remain

--- src/pages/Authenticator/Authenticator.tsx
+++ src/pages/Authenticator/Authenticator.tsx
@@ -15,36 +15,40 @@ const Authenticator = () => {
         mobile: "",
         device_type: "0"
     }),
-        { first_name, last_name, email, password, mobile } = state,
+        [showSignIn, setShowSignIn] = useState<boolean>(false),
+        { first_name, last_name, email, password, mobile } = state;
 
-        formFields = [
-            {
-                name: 'first_name',
-                value: first_name,
-                label: 'First Name',
-            },
-            {
-                name: 'last_name',
-                value: last_name,
-                label: 'Last Name'
-            },
-            {
-                name: 'email',
-                value: email,
-                label: 'Email'
-            }, {
-                name: 'mobile',
-                value: mobile,
-                label: 'Contact No.',
-                type: 'number'
-            },
-            {
-                name: 'password',
-                value: password,
-                label: 'Password'
-            }
+    let formFields = [
+        {
+            name: 'first_name',
+            value: first_name,
+            label: 'First Name',
+        },
+        {
+            name: 'last_name',
+            value: last_name,
+            label: 'Last Name',
+        },
+        {
+            name: 'email',
+            value: email,
+            label: 'Email',
+        }, {
+            name: 'mobile',
+            value: mobile,
+            label: 'Contact No.',
+            type: 'number',
+        },
+        {
+            name: 'password',
+            value: password,
+            label: 'Password',
+        }
+    ];
 
-        ];
+    if (showSignIn) {
+        formFields = formFields.filter((input: any) => (input.name === 'email' || input.name === 'password'))
+    }
 
     const handleChange = (e: any) => {
         const { name, value } = e.target;
@@ -57,13 +61,19 @@ const Authenticator = () => {
 
     return (
         <div className="authenticator">
+            <h1>
+                {showSignIn ? "SignIn" : "SignUp"}
+            </h1>
             <form onSubmit={handleSubmit} className="authenticator">
                 {
                     formFields.map((inputData, i) => <InputBox key={`form-input-${i}`} {...inputData} handleChange={handleChange} required />)
                 }
                 <button>Submit</button>
             </form>
-
+            {
+                showSignIn ? <p>Don't have a account? <span onClick={() => setShowSignIn(false)}>SignUp</span> here.</p>
+                    : <p>Already have an account? <span onClick={() => setShowSignIn(true)}>SignIn</span> here.</p>
+            }
         </div>
     )
 }
